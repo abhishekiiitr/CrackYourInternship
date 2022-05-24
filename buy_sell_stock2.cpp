28a82f8b536c7c4e3a72566afc37119b538bb568
abhishekiiitr <77018574+abhishekiiitr@users.noreply.github.com>
2022-05-23 20:50:48 -0700

    Create buy_sell_stock2.cpp
    buy sell stock 2

---
+++ buy_sell_stock2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int b=0;
+        for(int i=0 ; i < prices.size()-1; i++){
+            if(prices[i]<prices[i+1]) b+=prices[i+1]-prices[i];
+        }return b ;
+    } 
+};
