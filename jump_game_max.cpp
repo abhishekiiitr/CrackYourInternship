b65dd8bfbcf57be3f753eaab806430052b85e15d
abhishekiiitr <77018574+abhishekiiitr@users.noreply.github.com>
2022-05-25 06:24:40 -0700

    Create jump_game_max.cpp
    c++ code for max jump game

---
+++ jump_game_max.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int s=0;
+        for(int i=0 ; i<nums.size();i++){
+            if(s<i) return false;
+            s=max(s,i+nums[i]);
+        } return true;
+    }
+};
